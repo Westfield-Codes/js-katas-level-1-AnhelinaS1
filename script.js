a08bace0e97a659ddf093c8ace2a181e61422add
AnhelinaS1 <anhelina207742@student-schoolsofwestfield.org>
2025-02-13 17:52:16 +0000

    Done

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-/* Variable and Alert Katas */
-// These katas do not involve functions and are very basic. 
-
-/* COLOR KATAS */
-
 /* Var Alert */
-// make a string variable for a color, set it equal to your favorite 
-var color = "green";
-// alert the message" "My favorite color is " plus the color
-alert("My favorite color is: " + color);
-/* Var Alert Prompt */
-// make a string variable for color, prompt the user for their favorite
-// alert color " is a nice color!"
+// make a string variable named color, set it equal to your favorite color.
+var age = 15;
+var name1 = "Angie"
 
-/* Var Alert Prompt Conditional */
-// make a variable for color, prompt the user for their favorite
-// if user enters "black" alert "Black is not really a color." 
-// otherwise alert color " is a nice color!"
+// alert the message" "My favorite color is " plus the variable name, outside the quotes.
+
+/* Var Alert Prompt */
+// make a string variable for color, but prompt the user for their favorite color.
+// alert color plus " is a nice color!" - note the space in the quotes.
+var age =prompt("What is your age?");
+var name = prompt("What is your name?")
+alert(name1 + "is" + age + " years old ");
+
+
