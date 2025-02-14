1f980bef6f1de6a4d59383bdc85c077b6a99f6f6
AnhelinaS1 <anhelina207742@student-schoolsofwestfield.org>
2025-02-14 15:48:19 +0000

    ice cream

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-/* Var Alert */
-// make a string variable named color, set it equal to your favorite color.
-var age = 15;
-var name1 = "Angie"
-// alert the message" "My favorite color is " plus the variable name, outside the quotes.
 /* Var Alert Prompt */
 // make a string variable for color, but prompt the user for their favorite color.
+var color = prompt(" what is your favorite color?");
 // alert color plus " is a nice color!" - note the space in the quotes.
-var age =prompt("What is your age?");
-var name = prompt("What is your name?")
-alert(name1 + " is " + age + " years old ");
-
-
+alert(color + " is a nice color!");
