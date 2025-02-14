df82b5f35fb4707c5462a5ff92f4af6317547e77
AnhelinaS1 <anhelina207742@student-schoolsofwestfield.org>
2025-02-14 15:15:55 +0000

    age kata

--- script.js
+++ script.js
@@ -2,14 +2,12 @@
 // make a string variable named color, set it equal to your favorite color.
 var age = 15;
 var name1 = "Angie"
-
 // alert the message" "My favorite color is " plus the variable name, outside the quotes.
-
 /* Var Alert Prompt */
 // make a string variable for color, but prompt the user for their favorite color.
 // alert color plus " is a nice color!" - note the space in the quotes.
 var age =prompt("What is your age?");
 var name = prompt("What is your name?")
-alert(name1 + "is" + age + " years old ");
+alert(name1 + " is " + age + " years old ");
 
 
