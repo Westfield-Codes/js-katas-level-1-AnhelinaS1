95a745fba9152f74b6b8032d9875b085e70f3377
AnhelinaS1 <anhelina207742@student-schoolsofwestfield.org>
2025-02-14 10:50:56 -0500

    ice cream katas

--- done.js
+++ done.js
@@ -29,6 +29,14 @@ alert("My favorite color is: " + color);
 
 /* Prompt */
 
+/* Var Alert Prompt */
+// make a string variable for favorite ice cream flavor, prompt user for it
+var flavor=prompt("which ice cream flavor do you want?");
+// make an integer variable for number of scoops on the cone, prompt user for it
+var scoops = prompt("how many scoops do you want in the cone?");
+// alert "You want " scoops "scoops of " flavor
+alert("You want " + scoops + " scoops of " + flavor);
+
 
 /* Prompt with Conditional */
 
